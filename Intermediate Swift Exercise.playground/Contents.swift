588dca17e773e0bd8e2e818f643e63144fa07ef8
Pragati Pandey <pragatipandey@tothenew.com>
2020-02-23 21:35:21 +0530

    Exercise of Swift Intermediate

---
+++ Intermediate Swift Exercise.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+/*Write a function called siftBeans(fromGroceryList:) that takes a grocery list (as an array of strings) and “sifts out” the beans from the other groceries. The function should take one argument that has a parameter name called list, and it should return a named tuple of the type (beans: [String], otherGroceries: [String]).
+
+Hint:
+
+Here is an example of how you should be able to call your function and what the result should be:
+
+let result = siftBeans(fromGroceryList: ["green beans",
+
+                                         "milk",
+
+                                         "black beans",
+
+                                         "pinto beans",
+
+                                         "apples"])
+
+ 
+
+result.beans == ["green beans", "black beans", "pinto beans"] // true
+
+result.otherGroceries == ["milk", "apples"] // true” */
+
+
