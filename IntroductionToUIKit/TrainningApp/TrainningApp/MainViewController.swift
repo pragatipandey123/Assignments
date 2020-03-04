0df517a826e3a6d94d2d5ec0c681e9198e0602b6
Pragati Pandey <pragatipandey@tothenew.com>
2020-03-04 17:16:57 +0530

    Fianl Merge

---
+++ IntroductionToUIKit/TrainningApp/TrainningApp/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  TrainningApp
+//
+//  Created by Pragati Pandey on 04/03/20.
+//  Copyright © 2020 PragatiPandey. All rights reserved.
+//
+
+import UIKit
+
+/// Initial View Controller. Use Table View
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
